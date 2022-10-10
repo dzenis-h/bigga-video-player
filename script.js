c3aabb5b4347971114a3bd2db5dd35bead1f0172
Dzenis <dzenis.hankusic@gmail.com>
2022-10-10 20:11:20 +0200

    ES6 refactor + ReadMe file

--- script.js
+++ script.js
@@ -1,65 +1,54 @@
-const video = document.getElementById('video');
-const play = document.getElementById('play');
-const stop = document.getElementById('stop');
-const progress = document.getElementById('progress');
-const timestamp = document.getElementById('timestamp');
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
 
 // Play & pause video
-function toggleVideoStatus() {
-  if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
-  }
-}
+const toggleVideoStatus = () => {
+  video.paused ? video.play() : video.pause();
+};
 
-// update play/pause icon
-function updatePlayIcon() {
-  if (video.paused) {
-    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
-  } else {
-    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
-  }
-}
+// Update play/pause icon
+const updatePlayIcon = () => {
+  video.paused
+    ? (play.innerHTML = '<i class="fa fa-play fa-2x"></i>')
+    : (play.innerHTML = '<i class="fa fa-pause fa-2x"></i>');
+};
 
 // Update progress & timestamp
-function updateProgress() {
+const updateProgress = () => {
   progress.value = (video.currentTime / video.duration) * 100;
 
   // Get minutes
   let mins = Math.floor(video.currentTime / 60);
   if (mins < 10) {
-    mins = '0' + String(mins);
+    mins = "0" + String(mins);
   }
-
   // Get seconds
-  let secs = Math.floor(video.currentTime % 60);
-  if (secs < 10) {
-    secs = '0' + String(secs);
+  let seconds = Math.floor(video.currentTime % 60);
+  if (seconds < 10) {
+    seconds = "0" + String(secs);
   }
-
-  timestamp.innerHTML = `${mins}:${secs}`;
-}
+  timestamp.innerHTML = `${mins}:${seconds}`;
+};
 
 // Set video time to progress
-function setVideoProgress() {
+const setVideoProgress = () => {
   video.currentTime = (+progress.value * video.duration) / 100;
-}
+};
 
 // Stop video
-function stopVideo() {
+const stopVideo = () => {
   video.currentTime = 0;
   video.pause();
-}
+};
 
 // Event listeners
-video.addEventListener('click', toggleVideoStatus);
-video.addEventListener('pause', updatePlayIcon);
-video.addEventListener('play', updatePlayIcon);
-video.addEventListener('timeupdate', updateProgress);
-
-play.addEventListener('click', toggleVideoStatus);
-
-stop.addEventListener('click', stopVideo);
-
-progress.addEventListener('change', setVideoProgress);
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("timeupdate", updateProgress);
+play.addEventListener("click", toggleVideoStatus);
+stop.addEventListener("click", stopVideo);
+progress.addEventListener("change", setVideoProgress);
